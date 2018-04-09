9c4c965be4e9831c3100fe03d15b40dd2003a911
likly <569553290@qq.com>
2018-04-09 15:40:54 +0800

    section 3,practiceA. done

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,19 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        final List<String> list = object.get("value");
+        for (String item : list) {
+            Integer count = collectionA.get(item);
+            if(count != null){
+                collectionA.put(item,--count);
+            }
+        }
+        return collectionA;
     }
 }
